1ec8ba8dc5612fd220e057a5ebc82d23f8b6bb2e
Liucheng Zhu <13927181971@163.com>
2022-03-03 23:18:10 +0800

    error QuickSort

---
+++ Algorithm/QuickSort.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void swap(int &x, int &y)
+{
+    x^=y;
+    y^=x;
+    x^=y;
+}
+
+int Partition_1(int arr[], int low, int high)
+{
+    int Pivotloc=arr[low];
+    while (low<high)
+    {
+        while (low<high && arr[high]>=Pivotloc)
+            --high;
+        swap(arr[low],arr[high]);
+        while (low<high && arr[low]<=Pivotloc)
+            ++low;
+        swap(arr[low],arr[high]);
+    }
+    return low;
+    
+}
+
+void QSort(int* arr, int n, int low, int high)
+{
+    int Pivotloc;
+    if(low<high)
+    {
+        Pivotloc=Partition_1(arr,low,high);
+        QSort(arr,n,low,Pivotloc-1);
+        QSort(arr,n,Pivotloc+1,high);
+    }
+}
+
+void QuickSort(int* arr, int n)
+{
+    QSort(arr,n,0,n-1);
+}
+
+int main(void)
+{
+    int a[]={2,1,3,5,4,6,8,7,10,9};
+    int n=sizeof(a)/sizeof(int);
+    QuickSort(a,n);
+    for(int i=0; i<n; i++)
+    {
+        printf("%d,",a[i]);
+    }
+    return 0;
+}
