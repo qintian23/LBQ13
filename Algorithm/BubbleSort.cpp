0d7f12c7d5a031bc0fec4c9c48b3eb23724a3156
Liucheng Zhu <13927181971@163.com>
2022-03-03 23:03:26 +0800

    BubbleSort

---
+++ Algorithm/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void BubbleSort(int* a, int n)
+{
+    int i,j,temp;
+    int change=0;
+    for(i=n-1; i>=0 && change==0; i--)
+    {
+        change=1;
+        for(j=0; j<i; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                change=0;
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    int a[]={2,1,3,5,4,6,8,7,10,9};
+    int n=sizeof(a)/sizeof(int);
+    BubbleSort(a,n);
+    for(int i=0; i<n; i++)
+    {
+        printf("%d,",a[i]);
+    }
+    return 0;
+}
