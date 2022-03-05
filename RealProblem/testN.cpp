d55bd0bb3b892bed886d3a62ee5b1f37c5f4ef7a
Liucheng Zhu <13927181971@163.com>
2022-03-05 19:31:08 +0800

    quicksort

--- RealProblem/testN.cpp
+++ RealProblem/testN.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
     int i;
-      for(i=0;i<100;i++)
-	   if((i*i*i+i*5)/6>=1000)break;
-	   printf("%d\n",i);
-	return 0;
+    for(i=0;i<100;i++)
+        if((i*i*i+i*5)/6>=1000)break;
+    printf("%d\n",i);
+    return 0;
 } 
