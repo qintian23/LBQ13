037032a2ee8ab61ada9acef07546b179fda20a58
Liucheng Zhu <13927181971@163.com>
2022-03-05 19:57:49 +0800

    Baoli

---
+++ RealProblem/inc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count=0;
+    int data[3][100000];
+
+    cin>> n;
+    for(int i=0; i<3; i++)
+        for(int j=0; j<n; j++)
+            cin>> data[i][j];
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            for(int k=0; k<n; k++)
+            {
+                if(data[0][i] < data[1][j] && data[1][j] < data[2][k])
+                    count++;
+            }
+        }
+    }
+
+    cout<< count <<endl;
+
+    return 0;
+}
