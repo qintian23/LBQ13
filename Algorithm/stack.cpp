1bdb077d2af4452590f3550b0b1955e3d0a5baa3
Liucheng Zhu <13927181971@163.com>
2022-03-25 11:22:24 +0800

    栈

--- Algorithm/stack.cpp
+++ Algorithm/stack.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// 栈的初始化
+#define STACK_INIT_SIZE 10
+#define STACKINCREMENT 10
+#define OK 1
+#define ERROR 0
 
 typedef int T;
 
@@ -10,11 +17,56 @@ typedef struct
     int size; // 当前可用最大容量
 }sqStack;
 
-// 栈的初始化
-#define STACK
-
+int InitStack(sqStack* s);
+int Push(sqStack* s, T e);// 入栈操作
+int Pop(sqStack* s, T* e);// 出栈操作
 
 int main(int argc, char** argcv)
 {
+    sqStack s;
+    InitStack(&s);
+    printf("%d\n",s.size);
+
+    int data[]={2,3,1,4,6,5,7,9,8};
+    int len=sizeof(data)/sizeof(int);
+    for(int i=0; i<len; i++)
+        Push(&s,data[i]);
+    
+    int val;
+    Pop(&s,&val);
+    printf("%d\n",val);
+
     return 0;
+}
+
+int InitStack(sqStack* s)
+{
+    s->base=(T*)malloc(STACK_INIT_SIZE*sizeof(T));
+    if(!s->base) return ERROR;
+    s->top=s->base;
+    s->size=STACK_INIT_SIZE;
+    return OK;
+}
+
+// 入栈操作
+int Push(sqStack* s, T e)
+{
+   if(s->top-s->base >= s->size)
+   {
+       s->base=(T*)realloc(s->base,s->size+STACKINCREMENT*sizeof(T));
+       if(!s->base) return ERROR;
+       s->top=s->base+s->size;
+       s->size=s->size+STACKINCREMENT;
+   }
+   *(s->top)=e;
+   s->top++;
+   return OK;
+}
+
+// 出栈操作
+int Pop(sqStack* s, T* e)
+{
+    if(s->top==s->base) return ERROR; // 栈满
+    *e=*--(s->top);
+    return OK;
 }
