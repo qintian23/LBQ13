6dd7e3578efdb1370152c163690d9006ce1b0ebb
Liucheng Zhu <13927181971@163.com>
2022-03-28 17:42:01 +0800

    油漆面积

--- RealProblem/2017/油漆面积.cpp
+++ RealProblem/2017/油漆面积.cpp
@@ -1,76 +1,89 @@
-#include<stdio.h>
-#include<algorithm>
-
-const int N=10010;
-
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Line
+{
+    int x1,x2,h,f;//左右坐标，纵坐标高度，f=1为入边，f=-1为出边
+    Line(){}
+    Line(int _l,int _r,int _h,int _f):x1(_l),x2(_r),h(_h),f(_f){}
+    bool operator<(const Line &l)const
+    {
+        return h<l.h;
+    }
+};
+//线段树的定义
+struct SegTree
+{
+    int pl,pr,cnt,len; //左端点编号，右端点编号，入边的数目，两个端点之前被覆盖的长度
+    SegTree():cnt(0),len(0){}
+    SegTree *lson,*rson;
+};
+const int N = 10000;
 int n;
-int m;
-
-struct Segment//记录一个矩形的一条竖边
+int ans=0;
+int X[N<<1]; //记录所有的横坐标
+Line lines[N];
+SegTree *buildTree(int pl,int pr) //构建区间树
 {
-    int x,y1,y2;
-    int k;
-    bool operator<(const Segment &s)const
-    {
-        return x<s.x;
-    }
-}seg[2*N];
-
-struct Node//线段树节点
-{
-    int len,cnt;
-}tr[4*N];
-//当前节点，当前区间，要修改区间，要修改值
-void update(int node,int l,int r,int a,int b,int k)
-{
-    if(a>r||b<l) return;
-    if(a<=l&&b>=r||l==r)
-    {
-        tr[node].cnt+=k;
-        //修正当前区间被覆盖总长度
-        //如果被覆盖，被覆盖总长度就是区间长度
-        if(tr[node].cnt>0) tr[node].len=r-l+1;
-        //如果没被覆盖，且是叶子节点，说明没有子节点
-        //区间被覆盖总长度必然为0
-        else if(l==r) tr[node].len=0;
-        //如果没被覆盖，有子节点，说明没有被完全覆盖
-        //需要从子节点获得被覆盖总长度
-        else tr[node].len=tr[node<<1].len+tr[node<<1|1].len;
-    }
-    else {
-        int mid=l+((r-l)>>1);
-        update(node<<1,l,mid,a,b,k);
-        update(node<<1|1,mid+1,r,a,b,k);
-
-        if(tr[node].cnt>0) tr[node].len=r-l+1;
-        else if(l==r) tr[node].len=0;
-        else tr[node].len=tr[node<<1].len+tr[node<<1|1].len;
-    }
+    SegTree *t = new SegTree();
+    t->pl = pl;
+    t->pr = pr;
+    if(pl==pr) return t;
+    int mid = ((pl+pr)>>1);
+    t->lson = buildTree(pl,mid);
+    t->rson = buildTree(mid+1,pr);
+    return t;
 }
-
+void updateLength(SegTree *pTree,int tl, int tr)
+{
+    if(pTree->cnt) pTree->len = X[tr]-X[tl-1]; //将区间树上的端点反入到X中求得二维坐标上的实际横坐标
+    else if(tl==tr) pTree->len = 0;
+    else pTree->len = pTree->lson->len+pTree->rson->len; //负数的情况
+}
+void update(SegTree *tree,int pl,int pr,int value)
+{
+    int tl = tree->pl;
+    int tr = tree->pr;
+    if(pl<=tl&&pr>=tr)
+    {
+        tree->cnt+=value;
+        updateLength(tree,tl,tr);
+        return;
+    }
+    int m = (tl+tr)>>1;
+    if(pl<=m) update(tree->lson,pl,pr,value);
+    if(pr>m) update(tree->rson,pl,pr,value);
+    updateLength(tree,tl,tr);
+}
 int main()
 {
     scanf("%d",&n);
     int x1,y1,x2,y2;
-    for(int i=0;i<n;++i)
+    int index = 0;
+    for(int i=0;i<n;i++)
     {
-        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-        seg[m++]={x1,y1,y2,1};
-        seg[m++]={x2,y1,y2,-1};
+        scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+        X[index] = x1;
+        lines[index] = Line(x1,x2,y1,1);
+        index++;
+        X[index] = x2;
+        lines[index] = Line(x1,x2,y2,-1);//高度2
+        index++;
     }
-    std::sort(seg,seg+m);
-
-    int ans=0;
-    for(int i=0;i<m;++i)
+    sort(X,X+index); //所有横坐标点排序
+    sort(lines,lines+index); //所有扫描线排序
+    //离散化横坐标
+    int X_end = unique(X,X+index)-X; //去重返回的迭代器，指向去重后容器中不重复序列的最后一个元素下一个元素
+    SegTree *root = buildTree(1,X_end); //初始化线段树
+    //从低到高遍历扫描线
+    for(int i=0;i<index;i++)
     {
-    	//注意应从第二条竖线开始算
-        if(i>0) ans+=tr[1].len*(seg[i].x-seg[i-1].x);
-        //用根节点编号为1还是为0的方式存储树无所谓，
-        //只要写对初始区间范围和左右子节点求法即可
-        update(1,0,9999,seg[i].y1,seg[i].y2-1,seg[i].k);
+        int pl = lower_bound(X,X+X_end,lines[i].x1)-X; //二分查找记录下标
+        int pr = lower_bound(X,X+X_end,lines[i].x2)-X; //二分查找记录下标
+        update(root,pl+1,pr,lines[i].f);
+        ans+=root->len*(lines[i+1].h-lines[i].h);
     }
-    if(ans == 8458)
-        ans = 3796;
     printf("%d",ans);
     return 0;
 }
+
