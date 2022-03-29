67d26930ce719cb27f79b9e29b20b4a65d095e9c
Liucheng Zhu <13927181971@163.com>
2022-03-29 19:12:05 +0800

    time out

--- RealProblem/2017/k倍区间.cpp
+++ RealProblem/2017/k倍区间.cpp
@@ -2,50 +2,41 @@
 #include <string.h>
 using namespace std;
 
-const int ALEN=1e5+1;
+const int ALEN=1e5+2;
 
 int a[ALEN];
 int sum[ALEN];
 
 int main(int argc, char* argv[])
 {
-    memset(sum, 0, sizeof(sum)); // 初始化计算器
+    memset(sum, 0, sizeof(sum));
     int n,k;
     int count=0;
     cin>> n >> k;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin>> a[i];
-        if(a[i]%k==0) // 子序列长度为1的计数
-        {
-            sum[0]++;
-            // cout<< a[i] <<endl;
-        }
+        sum[i]=sum[i-1]+a[i];
+        // cout<< sum[i] <<endl;
+        if(sum[i]%k==0) count++;
     }
-    
-    for(int d=2; d<=n; d++)
+
+    for(int i=1; i<=n; i++)
     {
-        // 子序和
-        for(int i=0; i<n-d; i++)
+        for(int j=1; j<=n-i; j++)
         {
-            int temp=0;
-            for(int j=i; j<=i+d; j++)
+            if((sum[j+i]-sum[j])%k==0)
             {
-                temp+=a[j];
-            }
-            if(temp!=0 && temp%k==0)
-            {
-                // cout<< temp <<endl;
-                sum[d-1]++;
+              // cout<< (sum[j+i]-sum[j]) <<endl;
+                count++;
             }
         }
-    }
-    
-    for(int i=0; i<n; i++)
-    {
-        count+=sum[i];
     }
 
     cout<< count <<endl;
     return 0;
 }
+
+
+
+
