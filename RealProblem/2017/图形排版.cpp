d86432419d093fa14d948dbb46d3ea7ea47d3535
Liucheng Zhu <13927181971@163.com>
2022-04-03 17:39:04 +0800

    图形排版

--- RealProblem/2017/图形排版.cpp
+++ RealProblem/2017/图形排版.cpp
@@ -1,71 +1,53 @@
-#include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <algorithm>
+#include<iostream>
+#include<cstdio>
+#include<cmath>
 using namespace std;
-int M,N; // N张图片: 1 ~ 1e5; M宽度: 1~100
-const int MAX=1e5+5;
-int f[MAX];
-struct pic
+const int MX=1e5+5;
+int M,N;
+int w[MX],h[MX],t[MX];
+void attach(int i,int &W,int &H)
 {
-    int h,w;
-}pics[MAX];
-struct line
-{
-    int w,h;
-    line(){w=0,h=0;}
-    line(int _w, int _h)
-    {
-        w=_w;
-        h=_h;
-    }
-    line operator+(pic _p)
-    {
-        if(w+_p.w>M)
-        {
-            _p.h=ceil(1.0*_p.h*(M-w)/_p.w);
-            _p.w=M-w;
-        }
-        return line(w+_p.w,max(h,_p.h));
-    }
-    bool full()
-    {
-        return w==M;
-    }
-    void clr(){w=h=0;}
-};
-
-int push_till_full(line a, int k)
-{
-    for (; k <= N && a.full(); ++k)
-    {
-        a=a+pics[k];
-    }
-    return a.h+f[k];
+     
+    if(W+w[i]>M)
+        H=max(H,(int)ceil(1.0*h[i]*(M-W)/w[i])); 
+    else
+        H=max(H,h[i]);
+    W=min(M,W+w[i]);
 }
-
-int main(int argc, char* argv[])
+int calc(int i,int W,int H)
 {
-    cin>> M >> N;
-    for (int i = 0; i < N; i++)
-        cin>> pics[i].w >> pics[i].h;
-    for (int i = N; i >= 1; --i)
-        f[i]=push_till_full(line(0,0),i);
-
-    line a;
-    int per=0;
-    int ans=1e7;
-    for (int i = 1; i <= N; ++i)
+    while(i<N&&W<M)
+        attach(i++,W,H);
+    return H+t[i];
+}
+int main()
+{
+    scanf("%d%d",&M,&N);
+    for(int i=0;i<N;i++)
     {
-        int new_h=per+push_till_full(a,i+1);
-        ans=min(ans,new_h);
-        a=a+pics[i];
-        if(a.full())
+        scanf("%d%d",&w[i],&h[i]);
+    }
+    for(int i=N-1;i>=0;i--)
+    {
+        t[i]=calc(i,0,0);
+    }
+     
+    int res=t[0];
+    int tmp;
+    int pre_h=0;
+    int W=0,H=0;
+    for(int i=0;i<N;i++)
+    {
+        tmp=calc(i+1,W,H);
+        res=min(res,pre_h+tmp);
+        attach(i,W,H);
+        if(W==M)
         {
-            per+=a.h;
-            a.clr();
+            pre_h+=H;
+            W=0;
+            H=0;
         }
     }
-    cout<< ans  <<endl;
+    cout<<res<<endl;
     return 0;
 }
