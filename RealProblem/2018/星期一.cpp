19541396dcd57bfc3ea1d90f1185432768a2f4d9
Liucheng Zhu <13927181971@163.com>
2022-04-06 15:55:26 +0800

    星期一

---
+++ RealProblem/2018/星期一.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// 判断闰年
+bool isLeap(int x)
+{
+    return x%400==0 || (x%4==0 && x%100!=0);
+}
+int main()
+{
+    int t=0;
+    for (int i = 1901; i <= 2000; i++)
+        t = t + (isLeap(i) ? 366 : 365);
+        
+    // int res=(t+(7-1))/7; //进1法
+    int res=t/7; // 去尾法
+    cout<< res <<endl;
+    return 0;
+}
